0516305726f25986036613d270333e204b15e3ad
Ilan Cohen <ilanco@gmail.com>
2024-03-12 20:14:45 +0200

    add: last updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Ilan Cohen. Last updated ${new Date().toISOString()}.`,
       },
       prism: {
         theme: prismThemes.github,
