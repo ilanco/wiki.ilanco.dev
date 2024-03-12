7ab1f108b8272249118c551990fa4eb8ed2e6a2f
Ilan Cohen <ilanco@gmail.com>
2024-03-12 20:23:28 +0200

    fix: remove blogentry

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,13 +49,6 @@ const config = {
             'https://github.com/ilanco/wiki.ilanco.dev/tree/main/',
         },
         blog: false,
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
